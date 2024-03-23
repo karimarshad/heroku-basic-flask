ab2d3c4ee263090376deb0d147d2522e4ec66071
karimarshad <karimarshad@users.noreply.github.com>
2024-03-23 11:31:46 -0400

    sds

--- app.py
+++ app.py
@@ -18,13 +18,13 @@
 
 ##############################################
 ###
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, CORS
 import json
 import re
 #from flask_cors import CORS
 
 app = Flask(__name__)
-#CORS(app)
+CORS(app)
 
 def extract_numerical_value(conclusion):
     """Extracts numerical value from conclusion string."""
